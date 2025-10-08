7b7e40cac6f09ecbaa6161c4e9555584d47d57c0
MohamedSalah54 <mohamed.salah199988.com>
2025-10-08 17:40:02 +0300

    fixes

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,38 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-import * as cookieParser from 'cookie-parser';
-import { join } from 'path';
-import { NestExpressApplication } from '@nestjs/platform-express';
+  import { ValidationPipe } from '@nestjs/common';
+  import { NestFactory } from '@nestjs/core';
+  import { AppModule } from './app.module';
+  import * as dotenv from 'dotenv';
+  import * as cookieParser from 'cookie-parser';
+  import { join } from 'path';
+  import { NestExpressApplication } from '@nestjs/platform-express';
 
-dotenv.config();
+  dotenv.config();
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  async function bootstrap() {
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-app.enableCors({
-  origin: [
-    'https://frontend-task-manager-app-pi.vercel.app/', 
-    'http://localhost:3000',
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-});
-
-
-  app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser());
-
-  app.useStaticAssets(join(__dirname, '..', 'public'), {
-    prefix: '/static/',
+  app.enableCors({
+    origin: [
+      'https://frontend-task-manager-app-pi.vercel.app', 
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
 
-  const PORT = process.env.PORT || 3001;
-  await app.listen(PORT, '0.0.0.0'); 
-}
 
-bootstrap();
+    app.useGlobalPipes(new ValidationPipe());
+    app.use(cookieParser());
+
+    app.useStaticAssets(join(__dirname, '..', 'public'), {
+      prefix: '/static/',
+    });
+
+    const PORT = process.env.PORT || 3001;
+    await app.listen(PORT, '0.0.0.0'); 
+  }
+
+  bootstrap();
